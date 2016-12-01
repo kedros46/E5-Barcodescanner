eb76d0faa94685094aa549d7bfc81b4aee27c38c
kedros46 <brecht46@gmail.com>
2016-12-01 14:51:16 +0100

    latest commit 15h

--- CameraGap/www/js/barcodescanner.js
+++ CameraGap/www/js/barcodescanner.js
@@ -16,29 +16,38 @@ function findProduct(barcode){
     alert("We got a barcode\nResult: " + barcode);
     $.ajax({
         type: "get",
-        url: "backend/handle-request.php/findProducts",
+        url: "https://drcnetwork.com/handle-request.php/findProducts",
         data: {
             barcode: barcode
         },
         datatype: "json",
         timeout: 2000
     }).done(function (data) {
-        alert(data);
+        if(data){
+            $("#results").html(processData(data));
+        }else {
+            $("#results").html("<p>barcode not found</p>");
+        }
 
-        data =  (data == null)? "barcode not found" : data;
-        $("#results").html(data);
 
     }).fail(function (xhr, status, error) {
 
         alert("error happened: ");
-        alert(status);
-        alert(error);
-        console.log(xhr, status , error);
 
         $("#results").html("<p>error happened: " + xhr + "\n" + status + "\n" + error + "</p>");
 
     });
 }
+
+function processData(data) {
+    //[{id, name, barcode, price, size}]
+    var html = "";
+    data.map((element, index)=>{
+        html += "<p><span>element.NAME</span><span>element.SIZE</span><span>element.PRICE</span></p>";
+    });
+    return html;
+}
+
 
 // test barcode (flesje water)
 // 20490331
